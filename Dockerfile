412e97d23afbfa3c5bc3688dae5129d4be780e24
Kev744 <94228138+Kev744@users.noreply.github.com>
2023-06-23 20:56:07 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.7-alpine3.17
+
+ENV API_KEY=$API_KEY
+
+WORKDIR /app
+
+COPY requirement.txt /app
+
+RUN mkdir /templates
+
+COPY templates /app/templates
+
+RUN apk update && apk upgrade && pip install --no-cache-dir --requirement requirement.txt
+
+COPY apimeteo.py /app
+
+EXPOSE 5000
+
+CMD [ "python", "apimeteo.py" ]
---
+++ apimeteo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 11:52:48 2023
+
+@author: kevin
+"""
+
+import requests
+import os
+from flask import Flask, request, render_template, flash, redirect
+app = Flask(__name__)
+app.secret_key = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
+
+
+
+
+@app.route("/", methods = ('GET','POST'))
+def home():
+    if request.method == 'POST':
+        lat = request.form['lat']
+        long = request.form['long']
+
+        if not lat:
+            flash('Fill out the latitude')
+        elif not(lat.replace(".","",1).replace("-", "", 1).isdigit()) :
+            flash("Correct the field lat in correct format")
+        elif not(abs(float(lat)) <= 90.0):
+            flash("The latitude must be between -90° and 90°")
+        elif not long:
+            flash('Fill out the longitude')
+        elif not(long.replace(".","",1).replace("-", "", 1).isdigit()) :
+            flash("Correct the field long in correct format")
+        elif not(abs(float(long)) <= 180.0):
+            flash("The longitude must be between -180° and 180°")
+        else:
+            return redirect(f'/weather?lat={lat}&long={long}') 
+    return render_template("create.html")
+
+@app.route('/weather')
+def weather():
+    lat = request.args.get('lat')
+    long = request.args.get('long')
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    return response.json()
+
+
+
+if __name__ == "__main__":
+    api_key = os.environ['API_KEY']
+    app.run(host = '0.0.0.0')
+    
